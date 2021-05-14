1bff5d2c1e772b5cc0b479bae0a7636d9d8e5fa8
Vadym Usov <onlinemovement@gmail.com>
2021-05-14 17:27:19 +0300

    adds getting the user's geolocation and function getWeatherByCoords

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react';
 
 // import s from './Weather.module.css';
-import { getWeatherByCity } from '../../weatherAPI/weatherAPI';
+import {
+  getWeatherByCity,
+  getWeatherByCoords,
+} from '../../weatherAPI/weatherAPI';
 import Form from '../Form';
 
 export default function Weather() {
-  //   const [coord, setCoord] = useState({});
+  const [coords, setCoords] = useState({});
 
   const [city, setCity] = useState('');
   const [weather, setWeather] = useState({});
@@ -14,6 +17,25 @@ export default function Weather() {
     if (!city) return;
     getWeatherByCity(city).then(setWeather);
   }, [city]);
+
+  useEffect(getLocation, [coords]);
+
+  function getLocation() {
+    const geolocation = navigator.geolocation;
+    geolocation.getCurrentPosition(showLocation, errorHandler);
+  }
+
+  async function showLocation({ coords }) {
+    const currentCoords = { lat: coords.latitude, lon: coords.longitude };
+    setCoords(currentCoords);
+
+    const weather = await getWeatherByCoords(currentCoords);
+    setWeather(weather);
+  }
+
+  function errorHandler(error) {
+    console.log(error);
+  }
 
   return (
     <div>
--- src/weatherAPI/weatherAPI.js
+++ src/weatherAPI/weatherAPI.js
@@ -1,6 +1,11 @@
 const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?';
 const API_KEY = 'e5f824df274533e5727ab610130f3ed2';
 
+function getWeatherByCoords({ lat, lon }) {
+  const url = `${BASE_URL}lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
+  return getWeater(url);
+}
+
 function getWeatherByCity(city) {
   const url = `${BASE_URL}q=${city}&units=metric&appid=${API_KEY}`;
   return getWeater(url);
@@ -10,4 +15,4 @@ function getWeater(url) {
   return fetch(url).then(res => res.json());
 }
 
-export { getWeatherByCity };
+export { getWeatherByCoords, getWeatherByCity };

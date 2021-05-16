b0151dffdd0e209a0e33bb7782036a341915911a
Vadym Usov <onlinemovement@gmail.com>
2021-05-16 15:17:43 +0300

    changes css styles

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -24,6 +24,7 @@ export default function Form({ setCity }) {
         value={inputCity}
         onChange={handleCityChange}
         className={s.input}
+        autoComplete="off"
       />
 
       <button type="submit" className={s.button}>
